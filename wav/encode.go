18443b0bb73a7ef71d74900937403cce75f77479
Your Name <you@example.com>
2023-03-21 17:34:29 +0100

    listener with wait detect sound to record

--- wav/encode.go
+++ wav/encode.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 
 	"github.com/brotholo/beep"
 	"github.com/pkg/errors"
@@ -99,33 +100,37 @@ func Encode(w io.WriteSeeker, s beep.Streamer, format beep.Format) (err error) {
 }
 
 type EncodePerpetum struct {
-	s       beep.Streamer
-	format  beep.Format
-	ask_ch  *chan bool
-	resp_ch *chan []byte
-	stop_ch *chan bool
-	headers *header
-	buff    *bytes.Buffer
-	file    *io.WriteSeeker
+	s        beep.Streamer
+	format   beep.Format
+	ask_ch   *chan bool
+	rbuff_ch *chan []byte
+	rtext_ch *chan string
+	stop_ch  *chan bool
+	headers  *header
+	buff     *bytes.Buffer
+	file     *io.WriteSeeker
 }
 
 func StartEncodePerpertum(
 	s beep.Streamer,
 	format beep.Format,
 	ask_ch *chan bool,
-	resp_ch *chan []byte,
+	rbuff_ch *chan []byte,
+	rtext_ch *chan string,
 	stop_ch *chan bool) bool {
 
 	ep := EncodePerpetum{}
 	ep.s = s
 	ep.format = format
 	ep.ask_ch = ask_ch
-	ep.resp_ch = resp_ch
+	ep.rbuff_ch = rbuff_ch
+	ep.rtext_ch = rtext_ch
 	ep.stop_ch = stop_ch
 	if !ep.EncodeSetup() {
 		return false
 	}
-	ep.Start()
+	//  ep.StartNoSilence()
+	ep.StartWithDetect()
 	return false
 }
 func (ep *EncodePerpetum) NewFile(filename string) *os.File {
@@ -136,12 +141,27 @@ func (ep *EncodePerpetum) NewFile(filename string) *os.File {
 	return wirgin
 }
 
-func (ep *EncodePerpetum) NewBuff() *bytes.Buffer {
+func (ep *EncodePerpetum) AddSilence(tbw *bufio.Writer, seconds int) int {
+	silence := [][2]float64{}
+	written := 0
+	for i := 0; i < 512; i += 1 {
+		silence = append(silence, [2]float64{0, 0})
+	}
+	for t := 0; t < (seconds * 16000 / 512); t += 1 {
+		_, written = ep.WriteSamples(tbw, 0, silence, len(silence))
+	}
+	return written
+
+}
+func (ep *EncodePerpetum) NewBuff() (*bytes.Buffer, *bufio.Writer, int) {
 	wirgin := bytes.NewBufferString("")
 	if err := binary.Write(wirgin, binary.LittleEndian, ep.headers); err != nil {
-		return nil
+		return nil, nil, 0
 	}
-	return wirgin
+	bw := bufio.NewWriter(wirgin)
+	//  new_written := ep.AddSilence(bw, 1)
+	new_written := 0
+	return wirgin, bw, new_written
 }
 func (ep *EncodePerpetum) EncodeSetup() bool {
 	if ep.format.NumChannels <= 0 {
@@ -176,10 +196,11 @@ func (ep *EncodePerpetum) GetHeaders() *header {
 }
 func (ep *EncodePerpetum) WriteSamples(
 	tbw *bufio.Writer,
-	buffer []byte,
 	written int,
 	samples [][2]float64,
 	nsamples int) (bool, int) {
+	fake_samples := make([][2]float64, 512)
+	buffer := make([]byte, len(fake_samples)*ep.format.Width())
 	buf := buffer
 	switch {
 	case ep.format.Precision == 1:
@@ -228,6 +249,7 @@ func (ep *EncodePerpetum) FinalizeDataFile(w *os.File, tbw *bufio.Writer, writte
 		return false
 	}
 	// finalize header
+	defer w.Close()
 	headers := ep.GetHeaders()
 	headers.FileSize = int32(44 + written) // 44 is the size of the header
 	headers.DataSize = int32(written)
@@ -243,90 +265,146 @@ func (ep *EncodePerpetum) FinalizeDataFile(w *os.File, tbw *bufio.Writer, writte
 		fmt.Println("ERROR FINALIZED DATA FILE seek end", err)
 		return false
 	}
-	w.Close()
 	fmt.Println("FILE FINALIZZATO", w.Name())
 	return true
 }
-func (ep *EncodePerpetum) Start() {
-	//  var (
-	short_buff := ep.NewBuff()
-	long_buff := ep.NewBuff()
-	//  filename := "debug_wav"
-	//  fn_count := 0
-	//  long_buff := ep.NewFile(filename + strconv.Itoa(fn_count) + ".wav")
 
-	bw := bufio.NewWriter(short_buff)
-	tbw := bufio.NewWriter(long_buff)
-	fake_samples := make([][2]float64, 512)
-	buffer := make([]byte, len(fake_samples)*ep.format.Width())
-	written := 0
-	twritten := 0
+type WakeUp struct {
+	tts              int
+	fake_break       int
+	fake_break_limit int
+	nsamples_rec     int
+	record_mode      bool
+	back_to_silence  int
+	threshold        float64
+	th_on            float64
+	th_off           float64
+	bottom_memory    [31][][2]float64
+}
 
+func InitWakeUp(tts int) *WakeUp {
+	wu := WakeUp{}
+	wu.tts = tts
+	wu.fake_break = 0
+	wu.fake_break_limit = 2
+	wu.back_to_silence = 0
+	wu.nsamples_rec = 0
+	wu.record_mode = false
+	wu.th_on = 0.12
+	wu.th_off = 0.12
+	wu.threshold = wu.th_off
+	wu.bottom_memory = [31][][2]float64{}
+	return &wu
+}
+
+func (wu *WakeUp) RefreshMem() [31][][2]float64 {
+	wb := wu.bottom_memory
+	wu.bottom_memory = [31][][2]float64{}
+	return wb
+
+}
+func (wu *WakeUp) BackupSamples(samples [][2]float64, nsamples int) {
+	for i := 0; i < 30; i += 1 {
+		wu.bottom_memory[i] = wu.bottom_memory[i+1]
+	}
+	wu.bottom_memory[30] = samples
+}
+func (wu *WakeUp) Check(samples [][2]float64, nsamples int) string {
+	current_svar := IsSilent(samples, wu.threshold, false, false)
+	switch {
+	case wu.record_mode:
+		fmt.Println(wu.back_to_silence, wu.tts)
+		if current_svar {
+			if wu.back_to_silence > wu.tts {
+				wu.record_mode = false
+				wu.back_to_silence = 0
+				fmt.Println("Back To Silent")
+				if wu.fake_break < wu.fake_break_limit {
+					return "drop"
+				}
+				wu.threshold = wu.th_off
+				return "complete"
+			} else {
+				wu.back_to_silence += 1
+				return "continue"
+			}
+		} else {
+			fmt.Println("RESET BACK TO SILENCE")
+			wu.back_to_silence = 0
+			wu.fake_break += 1
+			return "continue"
+		}
+	case !wu.record_mode:
+		if !current_svar {
+			fmt.Println("SILENCE BREAK")
+			wu.record_mode = true
+			wu.threshold = wu.th_on
+			return "init"
+		} else {
+			wu.BackupSamples(samples, nsamples)
+			return ""
+		}
+	}
+
+	return "complete"
+}
+
+func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool) {
+	long_buff, tbw, twritten := ep.NewBuff()
+	filename := "debug_wav"
+	fn_count := 0
+	wakeUp := InitWakeUp(60)
 	for {
-		select {
-		case <-*ep.stop_ch:
-			fmt.Println("STOP CHAN CALL")
-			samples, nsamples := ep.ReadSamples()
-			if samples == nil {
-				fmt.Println("TMP BUFF IS NIL")
-				return
-			}
-			ok, new_written := ep.WriteSamples(tbw, buffer, twritten, samples, nsamples)
-			if !ok {
-				fmt.Println("WRITE SAMPLES WRONG WRITE")
-				return
-			}
-			twritten = new_written
-			ep.FinalizeDataBuff(long_buff, tbw, twritten)
-			//  ep.FinalizeDataFile(long_buff, tbw, twritten)
-			//  fn_count += 1
-			//  long_buff = ep.NewFile(filename + strconv.Itoa(fn_count) + ".wav")
-			// NOT WORK YET
-			*ep.resp_ch <- long_buff.Bytes()
-			twritten = 0
-			long_buff = ep.NewBuff()
-			tbw = bufio.NewWriter(long_buff)
-			//  *ep.resp_ch <- make([]byte, 0)
-
-		case <-*ep.ask_ch:
-			fmt.Println("ASK CHAN CALL")
-			samples, nsamples := ep.ReadSamples()
-			if samples == nil {
-				fmt.Println("TMP BUFF IS NIL")
-				return
-			}
-			ok, new_written := ep.WriteSamples(bw, buffer, written, samples, nsamples)
-			if !ok {
-				fmt.Println("WRITE SAMPLES WRONG WRITE")
-				return
-			}
-			written = new_written
-			ok, new_twritten := ep.WriteSamples(tbw, buffer, twritten, samples, nsamples)
+		samples, nsamples := ep.ReadSamples()
+		if samples == nil {
+			fmt.Println("TMP BUFF IS NIL")
+			return
+		}
+		res := wakeUp.Check(samples, nsamples)
+		switch res {
+		case "complete":
+			fmt.Println("COMPLETE")
+			ok, new_twritten := ep.WriteSamples(tbw, twritten, samples, nsamples)
 			if !ok {
 				fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
 				return
 			}
 			twritten = new_twritten
-			ep.FinalizeDataBuff(short_buff, bw, written)
-			*ep.resp_ch <- short_buff.Bytes()
-			written = 0
-			//  fmt.Println("resetto il soft", short_buff.Len())
-			short_buff = ep.NewBuff()
-			bw = bufio.NewWriter(short_buff)
-			//  fmt.Println("POST resetto il soft", short_buff.Len())
-		default:
-			samples, nsamples := ep.ReadSamples()
-			if samples == nil {
-				fmt.Println("TMP BUFF IS NIL")
-				return
+			ep.FinalizeDataBuff(long_buff, tbw, twritten)
+			*ep.rbuff_ch <- long_buff.Bytes()
+			if debug_audio_file {
+				fn_count += 1
+				nfname := filename + strconv.Itoa(fn_count) + ".wav"
+				tmp_f := ep.NewFile(nfname)
+				tmp_f.Write(long_buff.Bytes())
+				tmp_f.Close()
+				*ep.rtext_ch <- nfname
 			}
-			ok, new_written := ep.WriteSamples(bw, buffer, written, samples, nsamples)
+
+			long_buff, tbw, twritten = ep.NewBuff()
+		case "init":
+			fmt.Println("INIT", twritten)
+			lsamples := wakeUp.RefreshMem()
+			for _, ss := range lsamples {
+				ok, new_twritten := ep.WriteSamples(tbw, twritten, ss, len(ss))
+				if !ok {
+					fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
+					return
+				}
+				twritten = new_twritten
+			}
+			ok, new_twritten := ep.WriteSamples(tbw, twritten, samples, nsamples)
 			if !ok {
-				fmt.Println("WRITE SAMPLES WRONG WRITE")
+				fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
 				return
 			}
-			written = new_written
-			ok, new_twritten := ep.WriteSamples(tbw, buffer, twritten, samples, nsamples)
+			twritten = new_twritten
+			fmt.Println("INIT DONE", twritten)
+		case "drop":
+			fmt.Println("DROP")
+			long_buff, tbw, twritten = ep.NewBuff()
+		case "continue":
+			ok, new_twritten := ep.WriteSamples(tbw, twritten, samples, nsamples)
 			if !ok {
 				fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
 				return
@@ -335,6 +413,24 @@ func (ep *EncodePerpetum) Start() {
 		}
 	}
 }
+
+func IsSilent(snd_data [][2]float64, threshold float64, logmin bool, logmax bool) bool {
+	max_sample := float64(0)
+	for _, s := range snd_data {
+		if s[0] > max_sample {
+			max_sample = s[0]
+		}
+	}
+	res := max_sample < threshold
+	if logmin && res {
+		fmt.Println("MAX SAMPLE", max_sample)
+	}
+	if logmax && !res {
+		fmt.Println("MAX SAMPLE", max_sample)
+	}
+	return res
+}
+
 func EncodeBuff(w io.Writer, s beep.Streamer, format beep.Format) (err error) {
 	defer func() {
 		if err != nil {
