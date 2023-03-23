7e3e61f6159e497ac38512e8227f2a1bc04b6213
Your Name <you@example.com>
2023-03-23 03:53:29 +0100

    .

--- wav/encode.go
+++ wav/encode.go
@@ -6,6 +6,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"strconv"
 
@@ -100,16 +101,20 @@ func Encode(w io.WriteSeeker, s beep.Streamer, format beep.Format) (err error) {
 }
 
 type EncodePerpetum struct {
-	s           beep.Streamer
-	format      beep.Format
-	ask_ch      *chan bool
-	rbuff_ch    *chan []byte
-	rtext_ch    *chan string
-	stop_ch     *chan bool
-	wakeup_time int
-	headers     *header
-	buff        *bytes.Buffer
-	file        *io.WriteSeeker
+	s                          beep.Streamer
+	format                     beep.Format
+	ask_ch                     *chan bool
+	rbuff_ch                   *chan []byte
+	rtext_ch                   *chan string
+	rsamples_ch                *chan [][][2]float64
+	stop_ch                    *chan bool
+	wakeup_time                int
+	min_vol_start_rec          float64
+	max_vol_stop_rec           float64
+	autobalance_start_stop_rec bool
+	headers                    *header
+	buff                       *bytes.Buffer
+	file                       *io.WriteSeeker
 }
 
 func StartEncodePerpertum(
@@ -118,9 +123,14 @@ func StartEncodePerpertum(
 	ask_ch *chan bool,
 	rbuff_ch *chan []byte,
 	rtext_ch *chan string,
+	rsamples_ch *chan [][][2]float64,
 	stop_ch *chan bool,
 	wakeup_time int,
-	debug_file bool) bool {
+	min_vol_start_rec float64,
+	max_vol_stop_rec float64,
+	autobalance_start_stop_rec bool,
+	debug_file bool,
+	debug_samples bool) bool {
 
 	ep := EncodePerpetum{}
 	ep.s = s
@@ -128,13 +138,17 @@ func StartEncodePerpertum(
 	ep.ask_ch = ask_ch
 	ep.rbuff_ch = rbuff_ch
 	ep.rtext_ch = rtext_ch
+	ep.rsamples_ch = rsamples_ch
 	ep.stop_ch = stop_ch
 	ep.wakeup_time = wakeup_time
+	ep.min_vol_start_rec = min_vol_start_rec
+	ep.max_vol_stop_rec = max_vol_stop_rec
+	ep.autobalance_start_stop_rec = autobalance_start_stop_rec
 	if !ep.EncodeSetup() {
 		return false
 	}
 	//  ep.StartNoSilence()
-	ep.StartWithDetect(debug_file)
+	ep.StartWithDetect(debug_file, debug_samples)
 	return false
 }
 func (ep *EncodePerpetum) NewFile(filename string) *os.File {
@@ -283,10 +297,12 @@ type WakeUp struct {
 	threshold        float64
 	th_on            float64
 	th_off           float64
+	autobalance      bool
 	bottom_memory    [31][][2]float64
+	complete_samples [][][2]float64
 }
 
-func InitWakeUp(tts int) *WakeUp {
+func InitWakeUp(tts int, th_on float64, th_off float64, autobalance bool) *WakeUp {
 	wu := WakeUp{}
 	wu.tts = tts
 	wu.fake_break = 0
@@ -294,10 +310,13 @@ func InitWakeUp(tts int) *WakeUp {
 	wu.back_to_silence = 0
 	wu.nsamples_rec = 0
 	wu.record_mode = false
-	wu.th_on = 0.12
-	wu.th_off = 0.12
+	wu.th_on = th_on
+	wu.th_off = th_off
+	wu.autobalance = autobalance
 	wu.threshold = wu.th_off
 	wu.bottom_memory = [31][][2]float64{}
+	//  wu.autobalance_buff = [][][2]float64{}
+	wu.complete_samples = [][][2]float64{}
 	return &wu
 }
 
@@ -313,11 +332,51 @@ func (wu *WakeUp) BackupSamples(samples [][2]float64, nsamples int) {
 	}
 	wu.bottom_memory[30] = samples
 }
+func (wu *WakeUp) CheckAutobalance(samples [][2]float64, nsamples int) string {
+	current_svar := IsSilent(samples, wu.threshold, false, false)
+	switch {
+	case wu.record_mode:
+		//  fmt.Println(wu.back_to_silence, wu.tts)
+		if current_svar {
+			//  if wu.back_to_silence > wu.tts {
+			//  wu.record_mode = false
+			//  wu.back_to_silence = 0
+			//  fmt.Println("Back To Silent")
+			//  if wu.fake_break < wu.fake_break_limit {
+			//  return "drop"
+			//  }
+			//  wu.threshold = wu.th_off
+			//  return "complete"
+			//  } else {
+			//  wu.back_to_silence += 1
+			//  return "continue"
+			//  }
+		} else {
+			//  fmt.Println("RESET BACK TO SILENCE")
+			//  wu.back_to_silence = 0
+			//  wu.fake_break += 1
+			//  return "continue"
+		}
+	case !wu.record_mode:
+		//  if !current_svar {
+		//  fmt.Println("SILENCE BREAK")
+		//  wu.record_mode = true
+		//  wu.threshold = wu.th_on
+		//  return "init"
+		//  } else {
+		//  wu.BackupSamples(samples, nsamples)
+		//  return ""
+		//  }
+	}
+
+	return "complete"
+}
 func (wu *WakeUp) Check(samples [][2]float64, nsamples int) string {
 	current_svar := IsSilent(samples, wu.threshold, false, false)
 	switch {
 	case wu.record_mode:
-		fmt.Println(wu.back_to_silence, wu.tts)
+		//  fmt.Println(wu.back_to_silence, wu.tts)
+		//  wu.complete_samples = append(wu.complete_samples, samples)
 		if current_svar {
 			if wu.back_to_silence > wu.tts {
 				wu.record_mode = false
@@ -353,22 +412,33 @@ func (wu *WakeUp) Check(samples [][2]float64, nsamples int) string {
 	return "complete"
 }
 
-func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool) {
+func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool, debug_samples bool) {
 	long_buff, tbw, twritten := ep.NewBuff()
 	filename := "debug_wav"
 	fn_count := 0
-	wakeUp := InitWakeUp(ep.wakeup_time)
+	wakeUp := InitWakeUp(ep.wakeup_time,
+		ep.min_vol_start_rec,
+		ep.max_vol_stop_rec,
+		ep.autobalance_start_stop_rec)
 	for {
 		samples, nsamples := ep.ReadSamples()
 		if samples == nil {
 			fmt.Println("TMP BUFF IS NIL")
 			return
 		}
-		res := wakeUp.Check(samples, nsamples)
+		var res string
+		//  if ep.autobalance_start_stop_rec {
+		//  res = wakeUp.CheckAutobalance(samples, nsamples)
+		//  } else {
+		res = wakeUp.Check(samples, nsamples)
+		//  }
 		switch res {
 		case "complete":
 			fmt.Println("COMPLETE")
 			ok, new_twritten := ep.WriteSamples(tbw, twritten, samples, nsamples)
+			if ep.autobalance_start_stop_rec {
+				wakeUp.complete_samples = append(wakeUp.complete_samples, samples)
+			}
 			if !ok {
 				fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
 				return
@@ -376,6 +446,12 @@ func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool) {
 			twritten = new_twritten
 			ep.FinalizeDataBuff(long_buff, tbw, twritten)
 			*ep.rbuff_ch <- long_buff.Bytes()
+			if debug_samples {
+				*ep.rsamples_ch <- wakeUp.complete_samples
+			}
+			if ep.autobalance_start_stop_rec {
+				wakeUp.complete_samples = [][][2]float64{}
+			}
 			if debug_audio_file {
 				fn_count += 1
 				nfname := filename + strconv.Itoa(fn_count) + ".wav"
@@ -391,6 +467,9 @@ func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool) {
 			lsamples := wakeUp.RefreshMem()
 			for _, ss := range lsamples {
 				ok, new_twritten := ep.WriteSamples(tbw, twritten, ss, len(ss))
+				if ep.autobalance_start_stop_rec {
+					wakeUp.complete_samples = append(wakeUp.complete_samples, samples)
+				}
 				if !ok {
 					fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
 					return
@@ -398,6 +477,9 @@ func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool) {
 				twritten = new_twritten
 			}
 			ok, new_twritten := ep.WriteSamples(tbw, twritten, samples, nsamples)
+			if ep.autobalance_start_stop_rec {
+				wakeUp.complete_samples = append(wakeUp.complete_samples, samples)
+			}
 			if !ok {
 				fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
 				return
@@ -409,6 +491,9 @@ func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool) {
 			long_buff, tbw, twritten = ep.NewBuff()
 		case "continue":
 			ok, new_twritten := ep.WriteSamples(tbw, twritten, samples, nsamples)
+			if ep.autobalance_start_stop_rec {
+				wakeUp.complete_samples = append(wakeUp.complete_samples, samples)
+			}
 			if !ok {
 				fmt.Println("WRITE SAMPLES WRONG WRITE TBW")
 				return
@@ -418,14 +503,20 @@ func (ep *EncodePerpetum) StartWithDetect(debug_audio_file bool) {
 	}
 }
 
-func IsSilent(snd_data [][2]float64, threshold float64, logmin bool, logmax bool) bool {
+func GetMaxValSample(snd_data [][2]float64) float64 {
 	max_sample := float64(0)
 	for _, s := range snd_data {
-		if s[0] > max_sample {
+		if math.Abs(s[0]) > max_sample {
 			max_sample = s[0]
 		}
 	}
+	//  fmt.Println(max_sample)
+	return max_sample
+}
+func IsSilent(snd_data [][2]float64, threshold float64, logmin bool, logmax bool) bool {
+	max_sample := GetMaxValSample(snd_data)
 	res := max_sample < threshold
+
 	if logmin && res {
 		fmt.Println("MAX SAMPLE", max_sample)
 	}
